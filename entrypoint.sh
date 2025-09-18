310e8b86eb224e83cdc887f70556754e4bad748a
Hien Dang <danghien2907@gmail.com>
2025-09-18 11:18:44 +0000

    Edit etcd

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,25 @@
 #!/bin/sh
 set -e
 
-echo "[entrypoint] Generating config.yaml from template + ENV..."
+echo "[entrypoint] Generating config.yaml from template..."
 
-sed "s|\${ETCD_HOST}|${ETCD_HOST}|g;
-     s|\${ETCD_USER}|${ETCD_USER}|g;
-     s|\${ETCD_PASSWORD}|${ETCD_PASSWORD}|g;
-     s|\${APISIX_ADMIN_API_KEY}|${APISIX_ADMIN_API_KEY}|g;
-     s|\${APISIX_NODE_LISTEN}|${APISIX_NODE_LISTEN}|g;
-     s|\${APISIX_SSL_LISTEN}|${APISIX_SSL_LISTEN}|g" \
+SSL_ENABLE_VAL=$([ "$APISIX_SSL_ENABLE" = "true" ] && echo true || echo false)
+
+sed "s|__ETCD_HOST__|${ETCD_HOST:-http://etcd-service:2379}|g;
+     s|__ETCD_USER__|${ETCD_USER:-root}|g;
+     s|__ETCD_PASSWORD__|${ETCD_PASSWORD:-L0caladm;;; }|g;
+     s|__APISIX_ADMIN_API_KEY__|${APISIX_ADMIN_API_KEY:-your-super-secret-key}|g;
+     s|__APISIX_SSL_ENABLE__|${SSL_ENABLE_VAL}|g;
+     s|__APISIX_SSL_LISTEN__|${APISIX_SSL_LISTEN:-9443}|g;" \
      /usr/local/apisix/conf/config-template.yaml > /usr/local/apisix/conf/config.yaml
+
+# xử lý node_listen thành YAML list
+NODE_LISTEN_VAL=$(echo "${APISIX_NODE_LISTEN:-9080}" | awk -F',' '{for(i=1;i<=NF;i++) print "  - "$i}')
+sed -i "s|__APISIX_NODE_LISTEN__|PLACEHOLDER|g" /usr/local/apisix/conf/config.yaml
+sed -i "/PLACEHOLDER/r /dev/stdin" /usr/local/apisix/conf/config.yaml <<EOF
+$NODE_LISTEN_VAL
+EOF
+sed -i "s|PLACEHOLDER||g" /usr/local/apisix/conf/config.yaml
 
 echo "[entrypoint] ✅ config.yaml created"
 exec apisix start -c /usr/local/apisix/conf/config.yaml
