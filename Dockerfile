a514b87e864a78ba53c7b09d253cdafb0bf7f8af
Hien Dang <danghien2907@gmail.com>
2025-09-18 11:15:07 +0000

    Edit entrypoint

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,10 @@
 FROM apache/apisix:latest
 
 USER root
-
-# Copy entrypoint và mẫu config
 COPY entrypoint.sh /entrypoint.sh
 COPY config-template.yaml /usr/local/apisix/conf/config-template.yaml
-
 RUN chmod +x /entrypoint.sh
 
 EXPOSE 9080 9443 9180 9280
-
 ENTRYPOINT ["/entrypoint.sh"]
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,16 +3,14 @@ set -e
 
 echo "[entrypoint] Generating config.yaml from template + ENV..."
 
-# Tạo config.yaml từ template, thay các biến
-envsubst '\
-${ETCD_HOST} \
-${ETCD_USER} \
-${ETCD_PASSWORD} \
-${APISIX_ADMIN_API_KEY} \
-${APISIX_NODE_LISTEN} \
-${APISIX_SSL_LISTEN} \
-' < /usr/local/apisix/conf/config-template.yaml > /usr/local/apisix/conf/config.yaml
+sed "s|\${ETCD_HOST}|${ETCD_HOST}|g;
+     s|\${ETCD_USER}|${ETCD_USER}|g;
+     s|\${ETCD_PASSWORD}|${ETCD_PASSWORD}|g;
+     s|\${APISIX_ADMIN_API_KEY}|${APISIX_ADMIN_API_KEY}|g;
+     s|\${APISIX_NODE_LISTEN}|${APISIX_NODE_LISTEN}|g;
+     s|\${APISIX_SSL_LISTEN}|${APISIX_SSL_LISTEN}|g" \
+     /usr/local/apisix/conf/config-template.yaml > /usr/local/apisix/conf/config.yaml
 
-echo "[entrypoint] Starting APISIX CP"
+echo "[entrypoint] ✅ config.yaml created"
 exec apisix start -c /usr/local/apisix/conf/config.yaml
 
